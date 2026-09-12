e4bd1956e84e83411ddc88384e2e5e27c5bb5b6f
wlejon <jonny.brannum@gmail.com>
2026-09-12 10:14:30 -0500

    feat(codegen-brass): enable BCE, WBE, GVN-PRE, loop fusion, and array contraction in Brass backend

--- src/codegen-brass/brass_backend.cpp
+++ src/codegen-brass/brass_backend.cpp
@@ -64,6 +64,11 @@ bool BrassBackend::emitObject(const il::Module& module, const std::string& outpu
     options.enable_jump_threading = true;
     options.enable_trace_layout = true;
     options.enable_f64_demote = false;
+    options.enable_bce = true;
+    options.enable_wbe = true;
+    options.enable_gvn_pre = true;
+    options.enable_loop_fusion = true;
+    options.enable_array_contraction = true;
     options.enable_pic = sharedRuntime_;
     options.key_constants = module.keyConstants;
     options.entry_symbol = entrySymbol_;
